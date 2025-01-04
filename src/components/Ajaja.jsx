6f66ca58bb4225fa3b7bf6cdb066ea4e32abe902
industrikreatifnusantara <industrikreatifnusantara@gmail.com>
2025-01-04 13:58:27 +0700

    first commit

---
+++ src/components/Ajaja.jsx
@@ -0,0 +1,9 @@
+export function Abc () {
+    return ( 
+        <>
+        <p>muncul</p>
+        
+        
+        </>
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+export function Header () {
+    return ( 
+        <>
+        <header className="bg-pink-500">
+
+            ini woy 
+        </header>
+        
+        
+        </>
+    )
+}
+
+export function Gigi () {
+    return ( 
+        <>
+        
+        <p>Gigi Putih</p>
+        
+        </>
+    )
+}
